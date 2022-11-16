483bc5f6f3b5735fbb9d59757b33009642bd68ce
Marcello Bardus <marcellobardus8@gmail.com>
2022-11-16 12:21:15 +0100

    loading blockheader with ffi works

--- test/lib/EthereumHeader.t.sol
+++ test/lib/EthereumHeader.t.sol
@@ -9,14 +9,9 @@ import {EthereumHeader} from "../../src/lib/EthereumHeader.sol";
 
 contract EthereumHeaderLib_Test is Test {
     function test_decodeParentHash() public {
-        string[] memory inputs = new string[](3);
+        string[] memory inputs = new string[](2);
         inputs[0] = "node";
-        inputs[1] = "-n";
-        inputs[2] = "./helpers/fetch_header_rlp.js";
-        // inputs[1] = "-n";
-        // inputs[
-        //     2
-        // ] = "0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002676d000000000000000000000000000000000000000000000000000000000000";
+        inputs[1] = "./helpers/fetch_header_rlp.js";
 
         bytes memory res = vm.ffi(inputs);
         console.logBytes(res);
