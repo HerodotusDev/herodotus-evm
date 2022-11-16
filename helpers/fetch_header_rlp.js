745a961f5ef67459e9101c105ec90d6eef991971
Marcello Bardus <marcellobardus8@gmail.com>
2022-11-16 12:01:32 +0100

    updated header fetching helper

--- helpers/fetch_header_rlp.js
+++ helpers/fetch_header_rlp.js
@@ -1,8 +1,45 @@
 require("dotenv").config();
 const axios = require("axios");
+const RLP = require("rlp");
 
 async function main() {
-  const p = 0;
+  const { ALCHEMY_URL } = process.env;
+  if (!ALCHEMY_URL) throw new Error(`ALCHEMY_URL has not been provided`);
+
+  const blockNumber = 1000;
+  const rpcBody = {
+    jsonrpc: "2.0",
+    method: "eth_getBlockByNumber",
+    params: ["0x" + blockNumber.toString(16), false],
+    id: 0,
+  };
+  const rpcResponse = await axios.post(ALCHEMY_URL, JSON.stringify(rpcBody));
+  const header = rpcResponse.data.result;
+
+  const data = [
+    header.parentHash,
+    header.sha3Uncles,
+    header.miner,
+    header.stateRoot,
+    header.transactionsRoot,
+    header.receiptsRoot,
+    header.logsBloom,
+    header.difficulty,
+    header.number,
+    header.gasLimit,
+    header.gasUsed,
+    header.timestamp,
+    header.extraData,
+    header.mixHash,
+    header.nonce,
+  ];
+
+  if (header.baseFeePerGas) {
+    data.push(header.baseFeePerGas);
+  }
+
+  const headerRlp = "0x" + RLP.encode(data).toString("hex");
+  console.log(headerRlp);
 }
 
 main();
--- test/lib/EthereumHeader.t.sol
+++ test/lib/EthereumHeader.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 import {Test} from "forge-std/Test.sol";
+import {console} from "../helpers/console.sol";
 
 import {CheatCodes} from "../utils/CheatCodes.sol";
 import {EthereumHeader} from "../../src/lib/EthereumHeader.sol";
@@ -9,13 +10,16 @@ import {EthereumHeader} from "../../src/lib/EthereumHeader.sol";
 contract EthereumHeaderLib_Test is Test {
     function test_decodeParentHash() public {
         string[] memory inputs = new string[](3);
-        inputs[0] = "python3";
+        inputs[0] = "node";
         inputs[1] = "-n";
-        inputs[
-            2
-        ] = "0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002676d000000000000000000000000000000000000000000000000000000000000";
+        inputs[2] = "./helpers/fetch_header_rlp.js";
+        // inputs[1] = "-n";
+        // inputs[
+        //     2
+        // ] = "0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002676d000000000000000000000000000000000000000000000000000000000000";
 
         bytes memory res = vm.ffi(inputs);
-        string memory output = abi.decode(res, (string));
+        console.logBytes(res);
+        // string memory output = abi.decode(res, (string));
     }
 }
