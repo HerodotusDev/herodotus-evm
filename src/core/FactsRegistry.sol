d009391b056377188c7d134dc18160826384f9fa
Marcello Bardus <marcellobardus8@gmail.com>
2023-10-30 13:13:06 +0000

    Reduced amount of calldata required to verify MPT proof

--- src/core/FactsRegistry.sol
+++ src/core/FactsRegistry.sol
@@ -45,7 +45,7 @@ contract FactsRegistry {
         address account,
         uint16 accountFieldsToSave,
         Types.BlockHeaderProof calldata headerProof,
-        Types.AccountTrieProof calldata accountTrieProof
+        bytes[] calldata accountTrieProof
     ) external {
         // Verify the proof and decode the account fields
         (uint256 nonce, uint256 accountBalance, bytes32 codeHash, bytes32 storageRoot) = verifyAccount(account, headerProof, accountTrieProof);
@@ -77,7 +77,12 @@ contract FactsRegistry {
         );        
     }
 
-    function proveStorage(address account, uint256 blockNumber, bytes32 slot, Types.StorageSlotTrieProof calldata storageSlotTrieProof) external {
+    function proveStorage(
+        address account,
+        uint256 blockNumber,
+        bytes32 slot,
+        bytes[] calldata storageSlotTrieProof
+    ) external {
         // Verify the proof and decode the slot value
         bytes32 slotValue = verifyStorage(account, blockNumber, slot, storageSlotTrieProof);
         accountStorageSlotValues[account][blockNumber][slot] = slotValue;
@@ -87,39 +92,40 @@ contract FactsRegistry {
     function verifyAccount(        
         address account,
         Types.BlockHeaderProof calldata headerProof,
-        Types.AccountTrieProof calldata accountTrieProof
+        bytes[] calldata accountTrieProof
     ) public view returns(uint256 nonce, uint256 accountBalance, bytes32 codeHash, bytes32 storageRoot) {
         // Ensure provided header is a valid one by making sure it is committed in the HeadersStore MMR
         _verifyAccumulatedHeaderProof(headerProof);
 
         // Verify the account state proof
         bytes32 stateRoot = headerProof.provenBlockHeader.getStateRoot();
-        bool isAccountProofValid = SecureMerkleTrie.verifyInclusionProof(
+        bytes memory accountRLP = SecureMerkleTrie.get(
             abi.encode(account),
-            accountTrieProof.accountRLP,
-            accountTrieProof.trieProof,
+            accountTrieProof,
             stateRoot
         );
-        require(isAccountProofValid, "ERR_INVALID_ACCOUNT_PROOF");
-        RLPReader.RLPItem[] memory accountFields = accountTrieProof.accountRLP.toRLPItem().readList();
+        RLPReader.RLPItem[] memory accountFields = accountRLP.toRLPItem().readList();
 
         // Decode the account fields
         (nonce, accountBalance, codeHash, storageRoot) = _decodeAccountFields(accountFields);
     }
 
-    function verifyStorage(address account, uint256 blockNumber, bytes32 slot, Types.StorageSlotTrieProof calldata storageSlotTrieProof) public view returns(bytes32 slotValue) {
+    function verifyStorage(
+        address account,
+        uint256 blockNumber,
+        bytes32 slot,
+        bytes[] calldata storageSlotTrieProof
+    ) public view returns(bytes32 slotValue) {
         bytes32 storageRoot = accountField[account][blockNumber][Types.AccountFields.STORAGE_ROOT];
         require(storageRoot != bytes32(0), "ERR_EMPTY_STORAGE_ROOT");
 
-        bool isStorageProofValid = SecureMerkleTrie.verifyInclusionProof(
+        bytes memory slotValueRLP = SecureMerkleTrie.get(
             abi.encode(slot),
-            storageSlotTrieProof.slotValue,
-            storageSlotTrieProof.trieProof,
+            storageSlotTrieProof,
             storageRoot
         );
-        require(isStorageProofValid, "ERR_INVALID_STORAGE_PROOF");
 
-        bytes memory slotValueBytes = storageSlotTrieProof.slotValue;
+        bytes memory slotValueBytes = slotValueRLP.toRLPItem().readBytes();
         assembly {
             slotValue := mload(add(slotValueBytes, 32))
         }
--- src/lib/Types.sol
+++ src/lib/Types.sol
@@ -12,16 +12,6 @@ library Types {
         bytes provenBlockHeader;
     }
 
-    struct AccountTrieProof {
-        bytes[] trieProof;
-        bytes accountRLP; // TODO This might be not needed as the last element of the trieProof contains the accountRLP
-    }
-
-    struct StorageSlotTrieProof {
-        bytes[] trieProof;
-        bytes slotValue; // TODO This might be not needed as the last element of the trieProof contains the slotValue
-    }
-
     enum AccountFields {
         NONCE,
         BALANCE,
