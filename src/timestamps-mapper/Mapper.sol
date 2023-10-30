087b91aa2e1516c50ccd4189cd1dec197ebbbec6
Marcello Bardus <marcellobardus8@gmail.com>
2023-10-30 12:17:40 +0000

    Update remapper comments and fix some smaller issues

--- src/timestamps-mapper/Mapper.sol
+++ src/timestamps-mapper/Mapper.sol
@@ -33,13 +33,20 @@ contract TimestampToBlockNumberMapper {
         bytes32[] inclusionProof;
     }
 
+    /// @notice offset used to calculate memory keys in order to avoid collisions when memoizing
     uint256 private _HASHMAP_KEY_OFFSET = 0xDEFACED00000;
 
+    /// @notice headersProcessor is the address of the headers processor contract
     HeadersProcessor public immutable headersProcessor;
 
+    /// @notice mappersCount represents the number of mappers created
     uint256 public mappersCount;
-    mapping(uint256 => MapperInfo) public mappers; // TODO memento pattern
 
+    /// @notice mappers maps the mapper id to the mapper info
+    mapping(uint256 => MapperInfo) public mappers;
+
+    /// @notice constructor
+    /// @param _headersProcessor the address of the headers processor contract
     constructor(HeadersProcessor _headersProcessor) {
         headersProcessor = _headersProcessor;
     }
@@ -55,41 +62,51 @@ contract TimestampToBlockNumberMapper {
     }
 
     /// @notice appends a batch of headers to the remapping MMR
+    /// @notice Reindexing MMRs are always grown from their latest size, thus they're frontrunnable
     /// @param targettedMapId the id of the mapper to which the headers are appended
     /// @param lastPeaks the peaks of the grown remapping MMR
     /// @param headersWithProofs the headers with their proofs against the MMR managed by the headers processor
     function reindexBatch(uint256 targettedMapId, bytes32[] calldata lastPeaks, Types.BlockHeaderProof[] calldata headersWithProofs) external {
+        // Ensure that remapper exists at the given id
         uint256 mapperStartBlock = mappers[targettedMapId].startsFromBlock;
         require(mapperStartBlock != 0, "ERR_MAPPER_DOES_NOT_EXIST"); // TODO handle 0 start block
 
+        // Load latest remapper size and root from storage
         uint256 mapperLatestSize = mappers[targettedMapId].latestSize;
         bytes32 mapperLatestRoot = mappers[targettedMapId].mmrSizeToRoot[mapperLatestSize];
 
+        // Calculate the remapper number of leaves(number of blocks remapped) from the latest size
         uint256 mapperLeavesCount = StatelessMmrHelpers.mmrSizeToLeafCount(mapperLatestSize);
 
+        // Create a mutable in memory copy of the MMR state
         bytes32[] memory nextPeaks = lastPeaks;
         uint256 nextSize = mapperLatestSize;
         bytes32 nextRoot = mapperLatestRoot;
 
+        // Calculate the first block number that will be appended to the remapping MMR, given it's current state
         uint256 firstBlockAppended = Math.max(mapperStartBlock, mapperStartBlock + mapperLeavesCount);
+        // Create mutable in memory copy of the first block number that will be appended to the remapping MMR
         uint256 nextExpectedBlockAppended = firstBlockAppended;
 
-        for(uint256 i = 0 ; i < headersWithProofs.length; i++) {            
+        // Iterate over the headers with proofs
+        for(uint256 i = 0 ; i < headersWithProofs.length; i++) {         
             bytes32 root;
             uint256 elementsCount;
 
+            // Scoped in order to avoid stack too deep error
+            // The idea is to memoize the root and elements count for a given tree id, this way we avoid SLOADs
             {
                 bytes32 hashmapIndex = keccak256(abi.encodePacked(headersWithProofs[i].treeId + _HASHMAP_KEY_OFFSET)); // TODO use more efficient hash function
-                assembly ("memory-safe") { // TODO idk what memory-safe actually does
+                assembly ("memory-safe") {
                     root := mload(add(hashmapIndex, 32))
                     elementsCount := mload(add(hashmapIndex, 64))
                 }
 
                 // In this case SLOAD from HeaderProcessor is needed
                 if(root == bytes32(0)) {
-                    uint256 mmrSize = headersProcessor.getLatestMMRSize(headersWithProofs[i].treeId); // TODO wrong
-                    bytes32 mmrRoot = headersProcessor.getLatestMMRRoot(headersWithProofs[i].treeId); // TODO wrong
-                    assembly ("memory-safe") { // TODO idk what memory-safe actually does
+                    uint256 mmrSize = headersWithProofs[i].mmrTreeSize;
+                    bytes32 mmrRoot = headersProcessor.getMMRRoot(headersWithProofs[i].treeId, mmrSize);
+                    assembly ("memory-safe") {
                         root := mmrRoot
                         elementsCount := mmrSize
                         mstore(add(hashmapIndex, 32), mmrRoot)
@@ -99,6 +116,7 @@ contract TimestampToBlockNumberMapper {
                 require(root != bytes32(0), "ERR_INVALID_TREE_ID");
             }
 
+            // Verify the proof against the MMR root
             StatelessMmr.verifyProof(
                 headersWithProofs[i].blockProofLeafIndex,
                 keccak256(headersWithProofs[i].provenBlockHeader),
@@ -107,14 +125,22 @@ contract TimestampToBlockNumberMapper {
                 elementsCount,
                 root
             );
-
+            
+            // Verify that the block number of the proven header is the next expected one
             uint256 blockNumber = EVMHeaderRLP.getBlockNumber(headersWithProofs[i].provenBlockHeader);
             require(blockNumber == nextExpectedBlockAppended, "ERR_BLOCK_NUMBER_TOO_LOW");
+
+            // Decode the timestamp from the proven header
             uint256 timestamp = EVMHeaderRLP.getTimestamp(headersWithProofs[i].provenBlockHeader);
+
+            // Append the timestamp to the remapping MMR
             (nextSize, nextRoot, nextPeaks) = StatelessMmr.appendWithPeaksRetrieval(bytes32(timestamp), nextPeaks, nextSize, nextRoot);
+
+            // Increment the next expected block number
             nextExpectedBlockAppended++;
         }
 
+        // Update the remapper state
         mappers[targettedMapId].latestSize = nextSize;
         mappers[targettedMapId].mmrSizeToRoot[nextSize] = nextRoot;
     }
