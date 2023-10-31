22bf436402beb36cb530c623809a8f4a56da80dc
Marcello Bardus <marcellobardus8@gmail.com>
2023-10-31 16:29:24 +0000

    removed turbo dir

--- src/turbo/proving-slot-assignment/TurboAuctioningSystemProxy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.20;
-
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-
-contract TurboAuctioningSystemProxy is ERC1967Proxy {
-    constructor(address _implementation) ERC1967Proxy(_implementation, "")  {}
-}
