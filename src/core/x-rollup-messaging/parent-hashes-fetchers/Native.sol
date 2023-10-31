4a8e4c0fdeb7f057207705ce86cd93caae6c4274
Marcello Bardus <marcellobardus8@gmail.com>
2023-10-31 14:19:31 +0000

    Ethereum parent hash fetcher implementation

---
+++ src/core/x-rollup-messaging/parent-hashes-fetchers/Native.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import {IParentHashFetcher} from "../interfaces/IParentHashFetcher.sol";
+
+/// @title NativeParentHashesFetcher
+/// @notice Fetches parent hashes for the native chain
+/// @notice for example if deployed on Ethereum, it will fetch parent hashes from Ethereum
+contract NativeParentHashesFetcher is IParentHashFetcher {
+    function fetchParentHash(bytes memory ctx)
+        external
+        view
+        override
+        returns (uint256 fetchedForBlock, bytes32 parentHash)
+    {
+        (fetchedForBlock) = abi.decode(ctx, (uint256));
+        parentHash = blockhash(fetchedForBlock - 1);
+        require(parentHash != bytes32(0), "ERR_PARENT_HASH_NOT_AVAILABLE");
+    }
+
+    function chainId() external view override returns (uint256) {
+        return block.chainid;
+    }
+}
