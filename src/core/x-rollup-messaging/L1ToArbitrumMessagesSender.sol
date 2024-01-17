9fb2e28840028cd0bc2fbd5ec1af10465e566d0f
Pia <gayeongparkk@gmail.com>
2024-01-17 17:21:16 +0900

    update mesasge sender

--- src/core/x-rollup-messaging/L1ToArbitrumMessagesSender.sol
+++ src/core/x-rollup-messaging/L1ToArbitrumMessagesSender.sol
@@ -2,35 +2,27 @@
 pragma solidity ^0.8.19;
 
 import {AbstractMessagesSender} from "./AbstractMessagesSender.sol";
-import {IOutbox} from "./interfaces/IOutbox.sol";
+import {IArbitrumInbox} from "./interfaces/IArbitrumInbox.sol";
 import {ISharpProofsAggregatorsFactory} from "../interfaces/ISharpProofsAggregatorsFactory.sol";
 import {IParentHashFetcher} from "./interfaces/IParentHashFetcher.sol";
 
 contract L1ToArbitrumMessageSender is AbstractMessagesSender {
-    IOutbox public immutable outbox;
+    IArbitrumInbox public immutable arbitrumInbox;
 
     constructor(
         ISharpProofsAggregatorsFactory _proofsAggregatorsFactory,
         IParentHashFetcher _parentHashFetcher,
         address _l2Target,
-        IOutbox _outbox
+        IArbitrumInbox _arbitrumInbox
     ) AbstractMessagesSender(_proofsAggregatorsFactory, _parentHashFetcher, _l2Target) {
-        outbox = _outbox;
+        arbitrumInbox = _arbitrumInbox;
     }
     
     function _sendMessage(address _l2Target, bytes memory _data, bytes memory _xDomainMsgGasData) internal override {
-        (uint256 l2GasLimit) = abi.decode(_xDomainMsgGasData, (uint256));
-        // TODO : check if this is correct way to send message
-        outbox.executeTransaction{gas: l2GasLimit}(
-            new bytes32[](0),
-            0,
-            msg.sender,
-            _l2Target,
-            0,
-            0,
-            0,
-            0,
-            _data
+        (uint256 l2GasLimit, uint256 maxFeePerGas) = abi.decode(_xDomainMsgGasData, (uint256,uint256));
+        // TODO : check if this is correct way to send message to arbitrum
+        arbitrumInbox.createRetryableTicket(
+            _l2Target, 0, 0, msg.sender, msg.sender, l2GasLimit, maxFeePerGas, _data
         );
     }
 }
