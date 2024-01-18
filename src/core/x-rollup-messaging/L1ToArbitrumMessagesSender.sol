aae96228fa3c117c994460c7461c44fb337d8a72
Pia <gayeongparkk@gmail.com>
2024-01-18 13:33:30 +0900

    minor fix

--- src/core/x-rollup-messaging/L1ToArbitrumMessagesSender.sol
+++ src/core/x-rollup-messaging/L1ToArbitrumMessagesSender.sol
@@ -6,7 +6,7 @@ import {IArbitrumInbox} from "./interfaces/IArbitrumInbox.sol";
 import {ISharpProofsAggregatorsFactory} from "../interfaces/ISharpProofsAggregatorsFactory.sol";
 import {IParentHashFetcher} from "./interfaces/IParentHashFetcher.sol";
 
-contract L1ToArbitrumMessageSender is AbstractMessagesSender {
+contract L1ToArbitrumMessagesSender is AbstractMessagesSender {
     IArbitrumInbox public immutable arbitrumInbox;
 
     constructor(
@@ -20,7 +20,6 @@ contract L1ToArbitrumMessageSender is AbstractMessagesSender {
     
     function _sendMessage(address _l2Target, bytes memory _data, bytes memory _xDomainMsgGasData) internal override {
         (uint256 l2GasLimit, uint256 maxFeePerGas) = abi.decode(_xDomainMsgGasData, (uint256,uint256));
-        // TODO : check if this is correct way to send message to arbitrum
         arbitrumInbox.createRetryableTicket(
             _l2Target, 0, 0, msg.sender, msg.sender, l2GasLimit, maxFeePerGas, _data
         );
