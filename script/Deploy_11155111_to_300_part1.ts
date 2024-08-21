e0ce1dcefa87fcd4858299aa46be248066ae91a5
Filip Krawczyk <kotmawszy@gmail.com>
2024-08-22 00:16:33 +0200

    Clean up

--- script/Deploy_11155111_to_300_part1.ts
+++ script/Deploy_11155111_to_300_part1.ts
@@ -1,6 +1,4 @@
 import { Wallet } from "zksync-ethers";
-import * as ethers from "ethers";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 import dotenv from "dotenv";
 import hre from "hardhat";
