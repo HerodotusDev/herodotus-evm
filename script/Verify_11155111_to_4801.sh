2b36fb7180df68ecbc4785828c327278d58e9423
Mikolaj Jakobiak <jakobiakmikolaj@gmail.com>
2024-09-05 15:49:20 +0100

    added verification script for 11155111->4801 and deployment script for 4801->4801

---
+++ script/Verify_11155111_to_4801.sh
@@ -0,0 +1,28 @@
+source .env
+
+echo -e "Please enter contract addresses for the following contracts:\n"
+
+echo -n "HeadersStore: "
+read HEADERS_STORE
+
+echo -n "FactsRegistry: "
+read FACTS_REGISTRY
+
+echo -n "OpMessagesInbox: "
+read OP_MESSAGES_INBOX
+
+echo -n "NativeParentHashesFetcher: "
+read NATIVE_PARENT_HASHES_FETCHER
+
+echo -n "L1ToOptimismMessagesSender: "
+read L1_TO_OPTIMISM_MESSAGES_SENDER
+
+HEADERS_STORE=$HEADERS_STORE \
+    FACTS_REGISTRY=$FACTS_REGISTRY \
+    OP_MESSAGES_INBOX=$OP_MESSAGES_INBOX \
+    npx hardhat run script/Verify_11155111_to_4801_part1.ts --network worldChainSepolia
+
+OP_MESSAGES_INBOX=$OP_MESSAGES_INBOX \
+    NATIVE_PARENT_HASHES_FETCHER=$NATIVE_PARENT_HASHES_FETCHER \
+    L1_TO_OPTIMISM_MESSAGES_SENDER=$L1_TO_OPTIMISM_MESSAGES_SENDER \
+    npx hardhat run script/Verify_11155111_to_4801_part2.ts --network sepolia
